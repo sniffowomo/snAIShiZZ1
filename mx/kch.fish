23bfac1eebf7b1d06f253e3c4a2e467790a8cc1f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2024-03-13 23:18:13 +0000

    .

---
+++ mx/kch.fish
@@ -0,0 +1,10 @@
+#!/bin/fish
+/workspaces/m-TCJCFZTE24/mx/pnp.fish
+/workspaces/m-TCJCFZTE24/mx/fixrs.fish
+/workspaces/m-TCJCFZTE24/mx/st.sh
+/workspaces/m-TCJCFZTE24/mx/aryt.fish
+/workspaces/m-TCJCFZTE24/mx/tgpt.fish
+/workspaces/m-TCJCFZTE24/mx/bun.fish
+/workspaces/m-TCJCFZTE24/mx/tmuxx.fish
+
+
